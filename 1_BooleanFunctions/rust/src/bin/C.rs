893ce53aacc7a5a0fab56acfb2309cb7feb4fb1d
donRumata03 <donrumata03@gmail.com>
2021-10-15 10:31:10 +0300

    Solve C and E in Rust

---
+++ 1_BooleanFunctions/rust/src/bin/C.rs
@@ -0,0 +1,280 @@
+use std::{ops, io};
+use std::collections::{HashMap, HashSet};
+use std::fmt::{Display, Formatter};
+use std::fmt;
+use std::iter::FromIterator;
+use std::io::{stdin, BufRead, BufReader};
+use std::str;
+
+// Basic utils:
+
+struct BitSet {
+	mask: usize
+}
+impl BitSet {
+	fn from(msk: usize) -> BitSet {
+		BitSet {
+			mask: msk
+		}
+	}
+
+
+	fn get(&self, index: usize) -> bool {
+		(self.mask & (1 << index)) != 0
+	}
+
+
+	fn set(&mut self, position: usize, value: bool) {
+		if value {
+			self.set_true(position);
+		} else {
+			self.set_false(position);
+		}
+	}
+
+	fn set_true(&mut self, position: usize) {
+		self.mask |= (1_usize) << position;
+	}
+	fn set_false(&mut self, position: usize) {
+		self.mask &= !((1_usize) << position);
+	}
+}
+
+
+fn to_bin_vec(bitmask: usize, dims: usize) -> Vec<bool> {
+	let mut res = Vec::with_capacity(dims);
+
+	for i in 0..dims {
+		res.push(bitmask & (1 << i) != 0);
+	}
+
+	res
+}
+
+fn reverse_bitmask(bitmask: usize, dims: usize) -> usize {
+	let mut res = BitSet::from(0);
+
+	for i in 0..dims {
+		res.set(dims - i - 1, bitmask & (1 << i) != 0);
+	}
+
+	res.mask
+}
+
+fn to_bitmask(v: &Vec<bool>) -> usize {
+	let mut res: usize = 0;
+
+	for (i, value) in v.iter().enumerate() {
+		res |= (*value as usize) << i;
+	}
+
+	res
+}
+
+// _______________________          Bool Function           ___________________________
+
+struct BoolFunction {
+	data: Vec<bool>,
+	dims: usize
+}
+
+impl BoolFunction {
+	pub fn new(truth_table: Vec<bool>) -> BoolFunction {
+		let sz = truth_table.len();
+		BoolFunction {
+			data: truth_table,
+			dims: (sz as f32).log2() as usize
+		}
+	}
+
+	pub fn at(&self, index: usize) -> bool {
+		self.data[index]
+	}
+}
+
+impl ops::Index<usize> for BoolFunction {
+	type Output = bool;
+
+	fn index(&self, i: usize) -> &bool {
+		&self.data[i]
+	}
+}
+
+impl Display for BoolFunction {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		let mut repr_map = HashMap::new();
+
+		for (i, x) in self.data.iter().enumerate() {
+			repr_map.insert(to_bin_vec(i, self.dims), x);
+		}
+
+		write!(f, "{:?}", repr_map)
+	}
+}
+
+
+// _______________________          Scheme Node           ___________________________
+
+struct FuncNode {
+	inputs: Vec<usize>,
+	func: BoolFunction
+}
+impl FuncNode {
+	fn compute(&self, bitmask: usize) -> bool {
+		self.func[bitmask]
+	}
+}
+
+struct VarData {
+	element_index: usize,
+	var_index: usize
+}
+
+enum Node {
+	Variable(VarData), // Var's index
+	Function(FuncNode)
+}
+
+struct Scheme {
+	nodes: Vec<Node>
+}
+
+impl Scheme {
+	fn get_depth(&self, element: Option<usize>) -> usize {
+		match &self.nodes[element.unwrap_or(self.nodes.len()  - 1)] {
+			Node::Variable(_) => 0,
+
+			Node::Function(f_data) => f_data.inputs.iter().map(
+				|input_index| self.get_depth(Some(*input_index))
+			).max().unwrap() + 1
+		}
+	}
+
+	fn compute(&self, var_values: &BitSet) -> bool {
+		let mut element_results = vec![None; self.nodes.len()];
+
+		for (i, node) in self.nodes.iter().enumerate() {
+			match node {
+				Node::Variable(v) => element_results[i] = Some(var_values.get(v.var_index)),
+				Node::Function(func_node) => {
+					// Glue inputs:
+					for j in 0..i {
+						print!("{}", element_results[j].unwrap() as usize);
+					} println!();
+					let args: Vec<bool> =
+						func_node.inputs.iter().map(|i| element_results[*i].unwrap()).rev().collect();
+
+					let func_argument_bitmask: usize = to_bitmask(&args);
+
+					element_results[i] = Some(func_node.compute(func_argument_bitmask));
+				}
+			}
+		}
+
+		element_results.last().unwrap().unwrap()
+	}
+}
+
+// _____________        Scanner         ______________
+
+/// Reads white-space separated tokens one at a time.
+
+pub struct Scanner<R> {
+	reader: R,
+	buffer: Vec<String>,
+}
+
+impl<R: io::BufRead> Scanner<R> {
+	pub fn new(reader: R) -> Self {
+		Self {
+			reader,
+			buffer: vec![],
+		}
+	}
+
+	/// Use "turbofish" syntax token::<T>() to select data type of next token.
+	///
+	/// # Panics
+	///
+	/// Panics if there's an I/O error or if the token cannot be parsed as T.
+	pub fn token<T: str::FromStr>(&mut self) -> T {
+		loop {
+			if let Some(token) = self.buffer.pop() {
+				return token.parse().ok().expect("Failed parse");
+			}
+			let mut input = String::new();
+			self.reader.read_line(&mut input).expect("Failed read");
+			self.buffer = input.split_whitespace().rev().map(String::from).collect();
+		}
+	}
+}
+
+
+/// Main
+fn main() {
+	// println!("{}", reverse_bitmask(0b001, 3));
+
+	let mut scanner = Scanner::new(BufReader::new(io::stdin()));
+
+	// let boo: bool = scanner.token::<usize>() != 0;
+	// println!("{}", boo);
+
+	let n: usize = scanner.token();
+	assert!(1 <= n && n <= 27);
+
+	let mut nodes= Vec::new();
+
+	let mut var_counter = 0;
+	for i in 0..n {
+		let m_i: usize = scanner.token();
+		if m_i == 0 {
+			nodes.push(Node::Variable(VarData { element_index: i, var_index: var_counter }));
+			var_counter += 1;
+		} else {
+			// Read inputs:
+			let mut inputs = Vec::new();
+			for _ in 0..m_i {
+				inputs.push(scanner.token::<usize>() - 1);
+			}
+
+			// Read truth table:
+			let mut truth_table = Vec::new();
+			for _ in 0..2_usize.pow(m_i as u32) {
+				truth_table.push(scanner.token::<usize>() != 0);
+			}
+
+			nodes.push(Node::Function(FuncNode {
+				inputs,
+				func: BoolFunction::new(truth_table)
+			}));
+		}
+	}
+
+	let scheme = Scheme {
+		nodes
+	};
+
+	// Depth:
+	println!("{}", scheme.get_depth(None));
+
+	// Resultant truth table:
+	let tt_size = 2_usize.pow(var_counter as u32);
+	let mut res_string = String::with_capacity(tt_size);
+	for msk in 0..tt_size {
+		res_string.push(if scheme.compute(&BitSet::from(msk)) {'1'} else { '0' });
+	}
+	println!("{}", res_string);
+}
+
+/*
+
+5
+0
+0
+2 1 2
+1 1 0 1
+0
+2 3 4
+1 0 0 1
+
+ */
---
+++ 1_BooleanFunctions/rust/src/bin/E.rs
@@ -0,0 +1,216 @@
+use std::{ops, io};
+use std::collections::{HashMap, HashSet};
+use std::fmt::{Display, Formatter};
+use std::fmt;
+use std::iter::FromIterator;
+use std::io::{stdin, BufRead, BufReader};
+use std::str;
+
+// _______________________          Bin Vec Utils           ___________________________
+
+fn to_bin_vec(bitmask: usize, dims: usize) -> Vec<bool> {
+	let mut res = Vec::with_capacity(dims);
+
+	for i in 0..dims {
+		res.push(bitmask & (1 << i) != 0);
+	}
+
+	res
+}
+
+
+// _______________________          Bool Function           ___________________________
+
+struct BoolFunction {
+	data: Vec<bool>,
+	dims: usize
+}
+
+impl BoolFunction {
+	pub fn new(truth_table: Vec<bool>) -> BoolFunction {
+		let sz = truth_table.len();
+		BoolFunction {
+			data: truth_table,
+			dims: (sz as f32).log2() as usize
+		}
+	}
+
+	pub fn at(&self, index: usize) -> bool {
+		self.data[index]
+	}
+}
+
+impl ops::Index<usize> for BoolFunction {
+	type Output = bool;
+
+	fn index(&self, i: usize) -> &bool {
+		&self.data[i]
+	}
+}
+
+impl Display for BoolFunction {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		let mut repr_map = HashMap::new();
+
+		for (i, x) in self.data.iter().enumerate() {
+			repr_map.insert(to_bin_vec(i, self.dims), x);
+		}
+
+		write!(f, "{:?}", repr_map)
+	}
+}
+
+// _______________________          Zhegalkin Polynomial           ___________________________
+
+
+struct ZhegalkinPolynomial {
+	data: Vec<bool>,
+	dims: usize
+}
+
+impl ZhegalkinPolynomial {
+	pub fn new(coeffs: Vec<bool>) -> ZhegalkinPolynomial {
+		let sz = coeffs.len();
+		ZhegalkinPolynomial {
+			data: coeffs,
+			dims: (sz as f32).log2() as usize
+		}
+	}
+}
+
+impl ops::Index<usize> for ZhegalkinPolynomial {
+	type Output = bool;
+
+	fn index(&self, i: usize) -> &bool {
+		&self.data[i]
+	}
+}
+
+// mobius_transform
+
+fn get_slight_dominated(bitmask: usize) -> HashSet<usize> {
+	let mut res = HashSet::new();
+
+	let mut shift = 0usize;
+	while (1 << shift) <= bitmask {
+		if (1 << shift) & bitmask != 0 {
+			res.insert(!(1 << shift) & bitmask);
+		}
+		shift += 1;
+	}
+
+	res
+}
+
+// TODO: use the method of branches and bounds with dyn
+fn get_dominated_or_eq(bitmask: usize) -> Vec<usize> {
+	// let mut res = HashSet::new();
+	//
+	// for submsk in get_slight_dominated(bitmask).iter() {
+	// 	res.extend(get_dominated_or_eq(*submsk));
+	// }
+	// res.insert(bitmask);
+	//
+	// res
+
+	let mut res = Vec::new();
+
+	let mut s: isize = bitmask as isize;
+	while s > 0 {
+		res.push(s as usize);
+		s = (s-1) & (bitmask as isize);
+	}
+	res.push(0);
+	res
+}
+
+fn mobius_transform(bf: &BoolFunction) -> ZhegalkinPolynomial {
+	let n = bf.dims;
+
+	/*
+		let res_vec = vec![None; n];
+
+		for i in 0usize..(2usize.pow(n as u32)) {
+			if res_vec[i].is_some() { continue; }
+
+
+		}
+	*/
+
+	ZhegalkinPolynomial::new(
+		(0..(2usize.pow(n as u32))).map(
+			|bitmask| get_dominated_or_eq(bitmask).into_iter()
+				.map(|index| bf[index])
+				.fold(false, |a, b| a ^ b)
+		).collect()
+	)
+}
+
+/// Reads white-space separated tokens one at a time.
+pub struct Scanner<R> {
+	reader: R,
+	buffer: Vec<String>,
+}
+
+impl<R: io::BufRead> Scanner<R> {
+	pub fn new(reader: R) -> Self {
+		Self {
+			reader,
+			buffer: vec![],
+		}
+	}
+
+	/// Use "turbofish" syntax token::<T>() to select data type of next token.
+	///
+	/// # Panics
+	///
+	/// Panics if there's an I/O error or if the token cannot be parsed as T.
+	pub fn token<T: str::FromStr>(&mut self) -> T {
+		loop {
+			if let Some(token) = self.buffer.pop() {
+				return token.parse().ok().expect("Failed parse");
+			}
+			let mut input = String::new();
+			self.reader.read_line(&mut input).expect("Failed read");
+			self.buffer = input.split_whitespace().rev().map(String::from).collect();
+		}
+	}
+}
+
+fn main() {
+
+	// let xor = BoolFunction::new(vec![false, true, true, false]);
+	// println!("{}", xor[0b00]);
+	// println!("{}", xor[0b01]);
+	// println!("{}", xor[0b10]);
+	// println!("{}", xor[0b11]);
+	//
+	// println!("{}", xor);
+
+	// let mut dct = HashMap::new();
+	// dct.insert((false, false), false);
+	// dct.insert((false, true), true);
+	// dct.insert((true, false), true);
+	// dct.insert((true, true), false);
+	// println!("{:?}", dct);
+	//
+	// println!(get_dominated_or_eq())
+
+	let mut scanner = Scanner::new(BufReader::new(io::stdin()));
+
+
+	let n: usize = scanner.token();
+
+	let mut tt = Vec::new();
+	for _ in 0..(2usize.pow(n as u32)) {
+		scanner.token::<usize>();
+		let spl_r: usize = scanner.token();
+		tt.push(spl_r > 0);
+	}
+
+	let z = mobius_transform(&BoolFunction::new(tt));
+	for i in 0..(2usize.pow(n as u32)) {
+		println!("{:0>w$b} {:b}", i, z[i] as u32, w=n);
+	}
+
+}
