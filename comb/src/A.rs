2ae6259cc7d14d0a0c48e242d572543df843ac60
donRumata <donrumata03@gmail.com>
2022-01-05 12:27:19 +0300

    Add descriptor concept; pass A

--- comb/src/A.rs
+++ comb/src/A.rs
@@ -1,4 +1,253 @@
-use std::fmt::Debug;
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use {
+    std::{
+        io::{
+            self,
+            Read,
+            Write,
+            Stdin,
+            Stdout,
+            BufReader
+        },
+        fmt::{Display, Debug},
+        str,
+        cmp::min,
+        iter::once,
+        fs::{File, OpenOptions},
+    }
+};
+use std::collections::{HashMap, BinaryHeap};
+use std::cmp::Ordering;
+
+
+/// Writer
+pub struct OutputWriter<W: Write> {
+    writer: W,
+    buf: Vec<u8>,
+}
+
+impl OutputWriter<Stdout> {
+    pub fn new() -> Self { Self::from_writer(io::stdout()) }
+}
+
+impl OutputWriter<File> {
+    pub fn from_file(path: &str) -> Self {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(path);
+        Self::from_writer(file.unwrap())
+    }
+}
+
+impl<W: Write> OutputWriter<W> {
+    pub fn from_writer(writer: W) -> Self {
+        let buf = Vec::with_capacity(1 << 16);
+        Self { writer, buf }
+    }
+
+    pub fn print<T: Display>(&mut self, t: T) {
+        write!(self, "{}", t).unwrap();
+    }
+
+    pub fn prints<T: Display>(&mut self, t: T) {
+        write!(self, "{} ", t).unwrap();
+    }
+
+    pub fn println<T: Display>(&mut self, t: T) {
+        writeln!(self, "{}", t).unwrap();
+    }
+}
+
+impl<W: Write> Write for OutputWriter<W> {
+    fn write(&mut self, bytes: &[u8]) -> std::io::Result<usize> {
+        self.buf.extend(bytes);
+        Ok(bytes.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.writer.write_all(&self.buf)?;
+        self.writer.flush()?;
+        self.buf.clear();
+        Ok(())
+    }
+}
+
+impl<W: Write> Drop for OutputWriter<W> {
+    fn drop(&mut self) { self.flush().unwrap(); }
+}
+
+
+const EOF: &'static str = "InputReader: Reached end of input!";
+
+pub struct InputReader<R: Read> {
+    reader: R,
+    buf: Vec<u8>,
+    bytes_read: usize,
+    current_index: usize,
+}
+
+impl InputReader<Stdin> {
+    pub fn new() -> Self {
+        Self::from_reader(io::stdin())
+    }
+}
+
+impl InputReader<File> {
+    pub fn from_file(path: &str) -> Self {
+        Self::from_reader(File::open(path).unwrap())
+    }
+}
+
+impl<R: Read> InputReader<R> {
+    pub fn from_reader(reader: R) -> Self {
+        Self {
+            reader,
+            buf: vec![0; 1 << 16],
+            bytes_read: 0,
+            current_index: 0,
+        }
+    }
+
+    pub fn next<T: InputReadable>(&mut self) -> T {
+        T::from_input(self)
+    }
+
+    pub fn next_line(&mut self) -> String {
+        assert!(self.has_more(), EOF);
+        let mut line = String::new();
+        while self.peek() != '\n' {
+            line.push(self.peek());
+            self.consume();
+            if !self.has_more() { break; }
+        }
+        self.consume(); // consume '\n'
+        line
+    }
+
+    pub fn has_more(&mut self) -> bool {
+        if self.current_index >= self.bytes_read {
+            self.bytes_read = self.reader.read(&mut self.buf[..]).unwrap();
+            self.current_index = 0
+        }
+        self.bytes_read > 0
+    }
+
+    pub fn set_buf_size(&mut self, buf_size: usize) {
+        self.buf.resize(buf_size, 0);
+    }
+
+    fn peek(&self) -> char { self.buf[self.current_index] as char }
+
+    fn consume(&mut self) { self.current_index += 1; }
+
+    fn pop_digit(&mut self) -> u64 {
+        let c = self.peek();
+        self.consume();
+        c as u64 - '0' as u64
+    }
+
+    fn consume_until<F: Fn(char) -> bool>(&mut self, test: F) {
+        loop {
+            assert!(self.has_more(), EOF);
+            if test(self.peek()) { return; }
+            self.consume();
+        }
+    }
+
+    fn consume_until_sign(&mut self) -> i64 {
+        loop {
+            self.consume_until(|c| c.is_ascii_digit() || c == '-');
+            if self.peek() != '-' { return 1; }
+
+            self.consume();
+            assert!(self.has_more(), EOF);
+            if self.peek().is_ascii_digit() { return -1; }
+        }
+    }
+}
+
+pub trait InputReadable {
+    fn from_input<R: Read>(input: &mut InputReader<R>) -> Self;
+}
+
+impl InputReadable for u64 {
+    fn from_input<R: Read>(input: &mut InputReader<R>) -> Self {
+        input.consume_until(|c| c.is_ascii_digit());
+        let mut num = 0;
+        while input.peek().is_ascii_digit() {
+            num = num * 10 + input.pop_digit();
+            if !input.has_more() { break; }
+        }
+        num
+    }
+}
+
+impl InputReadable for i64 {
+    fn from_input<R: Read>(input: &mut InputReader<R>) -> Self {
+        let sign = input.consume_until_sign();
+        u64::from_input(input) as i64 * sign
+    }
+}
+
+impl InputReadable for f64 {
+    fn from_input<R: Read>(input: &mut InputReader<R>) -> Self {
+        let sign = input.consume_until_sign() as f64;
+        let mut num = 0.0;
+        while input.peek().is_ascii_digit() {
+            num = num * 10.0 + input.pop_digit() as f64;
+            if !input.has_more() { break; }
+        }
+
+        let mut factor = 1.0;
+        if input.peek() == '.' {
+            input.consume();
+            while input.has_more() && input.peek().is_ascii_digit() {
+                num = num * 10.0 + input.pop_digit() as f64;
+                factor *= 10.0;
+            }
+        }
+        sign * num / factor
+    }
+}
+
+impl InputReadable for String {
+    fn from_input<R: Read>(input: &mut InputReader<R>) -> Self {
+        input.consume_until(|c| c.is_ascii_graphic());
+        let mut word = String::new();
+        while input.peek().is_ascii_graphic() {
+            word.push(input.peek());
+            input.consume();
+            if !input.has_more() { break; }
+        }
+        word
+    }
+}
+
+impl InputReadable for char {
+    fn from_input<R: Read>(input: &mut InputReader<R>) -> Self {
+        input.consume_until(|c| c.is_ascii_graphic());
+        let c = input.peek();
+        input.consume();
+        c
+    }
+}
+
+macro_rules! impl_readable_from {
+  ($A:ty, [$($T:ty),+]) => {
+    $(impl InputReadable for $T {
+      fn from_input<R: Read>(input: &mut InputReader<R>) -> Self {
+        <$A>::from_input(input) as $T
+      }
+    })+
+  };
+}
+impl_readable_from!{ u64, [u32, u16, u8, usize] }
+impl_readable_from!{ i64, [i32, i16, i8, isize] }
+impl_readable_from!{ f64, [f32] }
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 trait CombinatorialObject<T> {
     fn is_self_contained(&self) -> bool;
@@ -20,7 +269,7 @@ fn generate_all<T, C>(prefix: &mut C, sorted_alphabet: &Vec<T>, answer_container
     where C: CombinatorialObject<T> + Clone + Debug,
             T: Clone
 {
-    println!("{:?}", prefix);
+    // println!("{:?}", prefix);
 
     if prefix.is_self_contained() {
         answer_container.push(prefix.clone())
@@ -38,30 +287,35 @@ fn generate_all<T, C>(prefix: &mut C, sorted_alphabet: &Vec<T>, answer_container
 
 
 #[derive(Clone, Debug)]
-struct Sequence {
-    len : usize,
-    states : u64,
-    data: Vec<u64>
+struct Sequence<'a> {
+    descriptor: &'a SequenceDescriptor,
+    data: Vec<u64>,
 }
 
-impl Sequence {
-    pub fn new(len: usize, states: u64) -> Self {
-        Sequence { len, states, data: Vec::with_capacity(len), /*sorted_alphabet: (1..=states).collect()*/ }
+#[derive(Clone, Debug)]
+struct SequenceDescriptor {
+    len : usize,
+    states : u64,
+}
+
+impl<'a> Sequence<'a> {
+    pub fn new(descriptor: &'a SequenceDescriptor) -> Self {
+        Sequence { descriptor, data: Vec::with_capacity(descriptor.len), /*sorted_alphabet: (1..=states).collect()*/ }
     }
 }
 
-impl CombinatorialObject<u64> for Sequence {
+impl CombinatorialObject<u64> for Sequence<'_> {
     fn is_self_contained(&self) -> bool {
-        self.data.len() == self.len
+        self.data.len() == self.descriptor.len
     }
 
     fn can_add(&self, _: &u64) -> bool {
-        self.data.len() < self.len
+        self.data.len() < self.descriptor.len
     }
 
     fn count_successors(&self) -> u64 {
-        return (self.states as u64)
-            .pow((self.len - self.data.len()) as u32);
+        return (self.descriptor.states as u64)
+            .pow((self.descriptor.len - self.data.len()) as u32);
     }
 
     fn push(&mut self, element: u64) {
@@ -77,12 +331,23 @@ impl CombinatorialObject<u64> for Sequence {
     // }
 }
 
+
 fn main() {
-    let mut pref = Sequence::new(3, 2);
+    let mut input = InputReader::new();
+    let mut output = OutputWriter::new();
+
+    let n = input.next();
+    let descriptor = SequenceDescriptor { len: n, states: 2 };
+    let mut pref = Sequence::new(&descriptor);
+
     let mut ans = Vec::with_capacity(pref.count_successors() as usize);
     let alphabet: Vec<u64> = (0..=1).collect();
 
     generate_all(&mut pref, &alphabet, &mut ans);
 
-    println!("{:?}", ans);
+    // println!("{:?}", ans);
+
+    for seq in ans.iter() {
+        output.println(seq.data.iter().map(u64::to_string).collect::<Vec<_>>().join(""));
+    }
 }
