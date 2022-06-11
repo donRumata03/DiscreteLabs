d4a7bf58a593b96a01cd013036069e5699ed8aac
donRumata <donrumata03@gmail.com>
2022-06-11 20:36:35 +0300

    Words DFA

---
+++ automata/words_DFA.py
@@ -0,0 +1,36 @@
+"""
+Count words of ANY length in DFA language
+
+© Vladimir Latypov™®
+"""
+
+file = open("problem3.in", "r")
+
+n, m, k, l = map(int, file.readline().split())
+terminal = list(map(int, file.readline().split()))
+# assert len(terminal) == k
+
+transitions = [dict() for i in range(n + 1)] # For all states keys are chars, values are lists of state indexes
+
+for i in range(m):
+	fr, to, c = file.readline().split()
+	fr = int(fr)
+	to = int(to)
+	transitions[fr][c] = to
+
+# For each state count ways from start stare to it
+
+# dp[i][j] is number of words (€ \Sigma^*) with length i such that this word consumption at automata leads to state j, j = 0 is nonsense, i = 0 is for empty word
+dp = [[0] * (n + 1)]
+dp[0][1] = 1
+
+for i in range(1, l + 1):
+	new_layer = [0] * (n + 1)
+
+	for source in range(n + 1):
+		for c in transitions[source]:
+			new_layer[transitions[source][c]] += dp[-1][source]
+
+	dp.append(new_layer)
+
+open("problem3.out", "w").write(str((sum([dp[-1][i] for i in range(n + 1) if i in terminal])) % (10**9 + 7)))
